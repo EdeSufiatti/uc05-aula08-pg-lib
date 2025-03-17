dca9bacc3090b834b26937165489cfe305a57355
Ede Sufiatti <sufiattie@gmail.com>
2025-03-16 21:38:54 -0300

    mudanças

--- src/instrutor/instrutor.controller.ts
+++ src/instrutor/instrutor.controller.ts
@@ -9,5 +9,26 @@ export class InstrutorController {
     this.service = service;
   }
 
+  async createInstrutor(req: Request<{}, {}, Instrutor>, res: Response) {
+    try {
+      const instrutor = req.body;
+      const novoInstrutor = await this.service.createInstrutor(instrutor);
+      res.status(201).json(instrutor);
+    } catch (error) {
+      res.status(400).json({ error: true,  message: error });
+    }
 
+  }
+
+ async getInstrutorById( _: Request<{ id: string }>, res: Response) {
+    try {
+      const instrutor = await this.service.getAll();
+      res.status(200).send(instrutor);
+    } catch (error) {
+      console.log("Error - InstrutorController>getInstrutorById", error);
+      res.status(500).send({ error: true, message: error });
+    }
+  }
+
+  
 }
